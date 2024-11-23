07b54ec51cfbd8cbbdcc68fe1b29608ed01c6665
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-11-23 22:18:00 +0530

    feat: add auth middleware

---
+++ server/src/middleware/auth-middkeware.ts
@@ -0,0 +1,44 @@
+import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
+import jwt from "jsonwebtoken";
+import { config } from "../config/config";
+
+interface AuthRequest extends Request {
+  user?: { id: string };
+}
+
+export function authMiddleware(
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return next(
+      createHttpError(401, "Authorization header is missing or invalid")
+    );
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decodedToken = jwt.verify(token, config.jwtSecret as string) as {
+      userId: string;
+    };
+
+    if (!decodedToken.userId) {
+      throw createHttpError(401, "Invalid token payload");
+    }
+
+    req.user = { id: decodedToken.userId };
+
+    next();
+  } catch (error) {
+    if (error instanceof jwt.JsonWebTokenError) {
+      return next(createHttpError(401, "Invalid or expired token"));
+    }
+
+    return next(createHttpError(500, "Internal server error"));
+  }
+}
