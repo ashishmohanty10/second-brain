3ad5577e34c9feaa4d77dbd7765dd5a6f011429a
ashishmohanty10 <ashishmohaty10@gmail.com>
2025-01-14 03:48:13 +0530

    feat: add update post route

--- server/src/controllers/user-controller.ts
+++ server/src/controllers/user-controller.ts
@@ -28,7 +28,7 @@ export async function createPost(
       if (existingPost)
         return next(createHttpError(400, "Post already exists"));
     }
-    await prisma.posts.create({
+    const newPost = await prisma.posts.create({
       data: {
         title,
         content,
@@ -45,6 +45,7 @@ export async function createPost(
 
     res.status(201).json({
       message: "Post created successfully",
+      post: newPost,
     });
   } catch (error) {
     console.log("--error while creating post", error);
@@ -63,6 +64,7 @@ export async function getAllPosts(
         userId: req.user?.id,
       },
       select: {
+        id: true,
         title: true,
         content: true,
         image: true,
@@ -99,6 +101,7 @@ export async function getPostById(
         id,
       },
       select: {
+        id: true,
         title: true,
         content: true,
         image: true,
@@ -119,5 +122,92 @@ export async function getPostById(
   } catch (error) {
     console.log("-error while getting posts by id", error);
     next(createHttpError(500, "Internal server error"));
+  }
+}
+
+export async function deletePost(
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) {
+  const { id } = req.body;
+
+  try {
+    const post = await prisma.posts.findUnique({
+      where: {
+        userId: req.user?.id,
+        id,
+      },
+    });
+
+    if (!post) return;
+
+    await prisma.posts.delete({
+      where: {
+        id,
+      },
+    });
+
+    res.status(200).json({
+      message: "Post deleted successfully",
+    });
+  } catch (error) {
+    console.log("--error while deleting a post", error);
+    next(createHttpError(500, "Internal server error "));
+  }
+}
+
+export async function updatePost(
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) {
+  const { id } = req.params;
+
+  if (!id) {
+    return next(createHttpError(400, "Post ID is required"));
+  }
+
+  const { title, link, tags, content, image } = req.body;
+
+  try {
+    const data = postSchema.safeParse({ title, link, tags, content, image });
+    if (!data.success) {
+      return next(createHttpError(400, "Invalid inputs"));
+    }
+
+    if (!Array.isArray(tags) || !tags.every((tag) => typeof tag === "string")) {
+      return next(createHttpError(400, "Tags must be an array of strings"));
+    }
+
+    const existingPost = await prisma.posts.findFirst({
+      where: { id: id, userId: req.user?.id },
+    });
+
+    if (!existingPost) {
+      return next(createHttpError(404, "Post not found"));
+    }
+
+    // Update the post
+    const updatedPost = await prisma.posts.update({
+      where: { id: id },
+      data: {
+        title,
+        content,
+        link,
+        image,
+        tags: {
+          connectOrCreate: tags.map((tagName: string) => ({
+            where: { tag: tagName },
+            create: { tag: tagName },
+          })),
+        },
+      },
+    });
+
+    res.status(200).json({ message: "Post updated successfully", updatedPost });
+  } catch (error) {
+    console.error("- Error while updating post:", error);
+    next(createHttpError(500, "Internal Server error"));
   }
 }
--- server/src/routes/v1/user-routes.ts
+++ server/src/routes/v1/user-routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import {
   createPost,
+  deletePost,
   getAllPosts,
   getPostById,
+  updatePost,
 } from "../../controllers/user-controller";
 import { authmiddleware } from "../../middleware/auth-middleware";
 
@@ -13,3 +15,5 @@ userRouter.post("/post", createPost);
 userRouter.get("/post", getAllPosts);
 
 userRouter.get("/post/:id", getPostById);
+userRouter.delete("/post", deletePost);
+userRouter.put("/post/:id", updatePost);
